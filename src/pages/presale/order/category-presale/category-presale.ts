bd780ecf30f62df173243c38e0732609d80ec5b3
Zulema Vicente <zulevp94@hotmail.com>
2017-05-29 12:07:46 -0400

    work with categories

--- src/pages/presale/order/category-presale/category-presale.ts
+++ src/pages/presale/order/category-presale/category-presale.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 
+import { TypeProductsService } from '../../../../providers/typeProducts';
+import { MarkProductsService } from '../../../../providers/markProducts';
+import { CategoryProductsService } from '../../../../providers/categoryProducts';
+
 @IonicPage({
   name: 'CategoryPresalePage',
   segment: 'category-presale/:order'
@@ -11,39 +15,9 @@ import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angu
 })
 export class CategoryPresalePage {
 
-  typeSelected: string = 'cuidado';
-  categories: any[] = [
-    {
-      title: 'Detergente en polvo',
-      value: 'beers',
-      image: 'https://cdn.galileo.pgsitecore.com/es-mx/-/media/Ariel_MX/Images/Products/Ariel-MB-1-kg-FT.png',
-      type: 'cuidado'
-    },
-    {
-      title: 'Detergente en barra',
-      image: 'http://industrialapopular.com/wp-content/uploads/2015/12/Barra-Detergente1.png',
-      type: 'cuidado',
-      value: 'food',
-    },
-    {
-      title: 'Soya',
-      image: 'http://ibescosocial.com/wordpress/wp-content/uploads/2016/03/soja-1.jpg',
-      type: 'alimentos',
-      value: 'beers',
-    },
-    {
-      title: 'Quinua',
-      image: 'http://guia.agrecolandes.org/wp-content/uploads/2016/04/quinua.png',
-      type: 'alimentos',
-      value: 'beers',
-    },
-    {
-      title: 'gaseosas',
-      image:'http://pizzali.net/wp-content/uploads/2014/08/40-138-large.jpg',
-      type: 'bebidas',
-      value: 'beers',
-    }
-  ];
+  typeSelected: string = 'all';
+  types: any[] = [];
+  categories: any[] = [];
   showCategories: any = [];
 
   order: string = '';
@@ -51,10 +25,24 @@ export class CategoryPresalePage {
   constructor(
     private navCtrl: NavController,
     private navParams: NavParams,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private categoriesService: CategoryProductsService,
+    private typesService: TypeProductsService
   ) {
-    this.showCategories = this.getCategories();
     this.order = this.navParams.get('order');
+  }
+
+  ionViewDidLoad(){
+    this.categoriesService.getAll()
+    .subscribe(categories =>{
+      this.categories = categories;
+      this.showCategories = this.categories;
+    });
+
+    this.typesService.getAll()
+    .subscribe(types =>{
+      this.types = types;
+    });
   }
 
   goToProductsPresalePage( category ){
@@ -64,7 +52,7 @@ export class CategoryPresalePage {
     });
   }
 
-  segmentChanged( event ){
+  onSelect( event ){
     this.typeSelected = event.value;
     this.showCategories = this.getCategories();
   }
@@ -78,9 +66,13 @@ export class CategoryPresalePage {
   }
 
   private getCategories(){
-    return this.categories.filter(item =>{
-      return item.type == this.typeSelected;
-    });
+    if(this.typeSelected == 'all'){
+      return this.categories;
+    }else{
+      return this.categories.filter(item =>{
+        return item.type == this.typeSelected;
+      });
+    }
   }
 
 }
--- src/providers/typeProducts.ts
+++ src/providers/typeProducts.ts
@@ -1,67 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable,  } from 'angularfire2/database';
 
-import * as firebase from 'firebase';
-
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
-
 @Injectable()
 export class TypeProductsService {
 
   typeProducts: FirebaseListObservable<any>;
-  productsRef: firebase.database.Query;
 
   constructor(
-    public fireDatabase: AngularFireDatabase,
-    public http: Http
+    public fireDatabase: AngularFireDatabase
   ) {
     this.typeProducts = this.fireDatabase.list('/typeProducts');
-    this.productsRef = this.typeProducts.$ref;
-  }
-
-  getData(){
-    return this.http.get('assets/json/data.json')
-    .map(response => response.json())
-    .toPromise();
-  }
-
-  getCategories(){
-    return this.http.get('assets/json/categories.json')
-    .map(response => response.json())
-    .toPromise();
-  }
-
-  getMark(){
-    return this.http.get('assets/json/mark.json')
-    .map(response => response.json())
-    .toPromise();
-  }
-
-  getGroup(){
-    return this.http.get('assets/json/group.json')
-    .map(response => response.json())
-    .toPromise();
   }
 
   getAll(){
     return this.typeProducts;
-  }
-
-  getProductsByCategory(category: string): Promise<any>{
-    return new Promise((resolve, reject)=>{
-      const query = this.productsRef.orderByChild('category').equalTo(category);
-      query.once('value', snap =>{
-        let data = snap.val();
-        if(data === null){
-          reject(data);
-        }else{
-          resolve(data);
-        }
-      })
-    })
-    
   }
   
   create(type){
